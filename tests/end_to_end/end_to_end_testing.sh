5f2bb620bdcdfb12ef1e248695567791d76fca2b
Челомов Степан <stepan.chelomov@mail.ru>
2024-10-17 23:41:47 +0300

    end to end tests gen

--- tests/end_to_end/end_to_end_testing.sh
+++ tests/end_to_end/end_to_end_testing.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-for file in *.in ; do 
-    echo $file Triangles that intersect:; ../../build/triangles < $file
+shopt -s nullglob
+files=(*.in)
+
+if [ ${#files[@]} -eq 0 ]; then
+    echo "Нет файлов с расширением .in."
+    exit 1
+fi
+
+for file in "${files[@]}"; do 
+    echo "Обработка файла: $file"
+    echo "Треугольники, которые пересекаются:"
+    ../../build/triangles < "$file"
+    echo "-----------------------------------"
 done
--- tests/end_to_end/generate_triangles.py
+++ tests/end_to_end/generate_triangles.py
@@ -1,23 +1,22 @@
-import random 
+import random
 
 tests_number = 10
 
-for test_number in range(0, tests_number):
-    name_of_file = "test_" + f'{test_number + 1:02}' + ".in"
-    file = open(name_of_file, 'w')
+for test_number in range(tests_number):
+    name_of_file = f"test_{test_number + 1:02}.in"
+    
+    with open(name_of_file, 'w') as file:
+        number_of_triangles = 10 
 
-    number_of_triangles = random.randint(100, 100)
+        test_text = str(number_of_triangles) + "\n"
 
-    test_text = str(number_of_triangles) + "\n"
+        for elem_number in range(number_of_triangles):
+            # Генерация трех точек (x, y, z) для каждого треугольника
+            first_point = (random.randint(-100, 100), random.randint(-100, 100), random.randint(-100, 100))
+            second_point = (random.randint(-100, 100), random.randint(-100, 100), random.randint(-100, 100))
+            third_point = (random.randint(-100, 100), random.randint(-100, 100), random.randint(-100, 100))
 
-    for elem_number in range(number_of_triangles) :
-        first_point  = random.normalvariate(0, 7)
-        second_point = random.normalvariate(3, 40)
-        third_point  = random.normalvariate(9, 100)
-        if elem_number % 50 == 0:
-            file.write(test_text)
-            test_text = ""
-        test_text += str(first_point)[:6] + " " + str(second_point)[:6] + " " + str(third_point)[:6] + " "
-    test_text += '\n'
-    file.write(test_text)
-    file.close()
+            formatted_triangle = f"{first_point[0]} {first_point[1]} {first_point[2]} " f"{second_point[0]} {second_point[1]} {second_point[2]} " f"{third_point[0]} {third_point[1]} {third_point[2]}\n"
+            test_text += formatted_triangle
+
+        file.write(test_text)
